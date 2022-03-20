e947c7d7a5d3bf952e305e61511c5b019d75f7e7
Enzear1 <101992132+Enzear1@users.noreply.github.com>
2022-03-20 21:33:48 +0300

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+chatUSERS = []
+total = 0
+chatDATA = {'total' : 0}
+
+router.get('/usersEvent',function(req,res) {
+    console.log(req.query)
+    if(req.query['eventName'] == 'addUserName') {
+        if(chatUSERS.includes(req.query['name'])) {
+            res.send('201')
+        } else {
+            chatUSERS.push(req.query['name'])
+            res.send('301')
+        }
+    } else if(req.query['eventName'] == 'sendMessage') {
+        total += 1
+        chatDATA['total'] = total
+        chatDATA[String(total)] = {
+            [req.query['name']] : req.query['msg']
+        }
+        console.log(chatUSERS)
+        console.log(chatDATA)
+        res.send('301')
+    }
+    console.log(chatUSERS)
+    console.log(chatDATA)
+})
+router.get('/getChat',function(req,res) {
+    res.send(chatDATA)
+})
+
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
