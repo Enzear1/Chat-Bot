6cf5abdb7986584e80992101961f4924c64fea63
Enzear1 <101992132+Enzear1@users.noreply.github.com>
2022-03-21 12:15:06 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -4,22 +4,21 @@ const router = express.Router();
 
 chatUSERS = []
 total = 0
-chatDATA = {'total' : 0}
+chatDATA = {}
 
-router.get('/usersEvent',function(req,res) {
+router.get('/usersEvent', function (req, res) {
     console.log(req.query)
-    if(req.query['eventName'] == 'addUserName') {
-        if(chatUSERS.includes(req.query['name'])) {
+    if (req.query['eventName'] == 'addUserName') {
+        if (chatUSERS.includes(req.query['name'])) {
             res.send('201')
         } else {
             chatUSERS.push(req.query['name'])
             res.send('301')
         }
-    } else if(req.query['eventName'] == 'sendMessage') {
+    } else if (req.query['eventName'] == 'sendMessage') {
         total += 1
-        chatDATA['total'] = total
         chatDATA[String(total)] = {
-            [req.query['name']] : req.query['msg']
+            [req.query['name']]: req.query['msg']
         }
         console.log(chatUSERS)
         console.log(chatDATA)
@@ -28,11 +27,11 @@ router.get('/usersEvent',function(req,res) {
     console.log(chatUSERS)
     console.log(chatDATA)
 })
-router.get('/getChat',function(req,res) {
+router.get('/getChat', function (req, res) {
     res.send(chatDATA)
 })
 
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(3344);
 
-console.log('Running at Port 3000');
+console.log('Running at Port 3344');
